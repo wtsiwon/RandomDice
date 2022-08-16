59b720d9d22f8109ff0430c4d5ed1f83467d6318
wtsiwon <siwon4727@gmail.com>
2022-08-16 13:44:58 +0900

    5X3배열 만듬

--- Assets/Script/DiceManager.cs
+++ Assets/Script/DiceManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DiceManager : Singleton<DiceManager>
 {
-    public Dice[,] diceGrid = new Dice[5, 5];
+    public Dice[,] diceGrid = new Dice[5, 3];
     public List<DiceData> dices = new List<DiceData>();
+
+    public List<int> posIndex = new List<int>();
+
 }
