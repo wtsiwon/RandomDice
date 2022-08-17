d622226d7096e0e2e032f59252f6874d05d9e55f
wtsiwon <siwon4727@gmail.com>
2022-08-17 16:17:09 +0900

    주사위 만듦

--- Assets/Script/DiceManager.cs
+++ Assets/Script/DiceManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class DiceManager : Singleton<DiceManager>
 {
     public Dice[,] diceGrid = new Dice[5, 3];
-    public List<DiceData> dices = new List<DiceData>();
 
-    public List<int> posIndex = new List<int>();
+    public List<DiceData> deck = new List<DiceData>();
+
+    public List<int> posIndex = new List<int>()
+    {
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+    };
 
 
 }
